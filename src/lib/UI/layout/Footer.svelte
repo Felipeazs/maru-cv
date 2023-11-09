642824b80e0354542ea58c1e820689bfdd937f5a
felipeazs <felipeazs@gmail.com>
2023-11-09 15:50:59 -0300

    cambio fondo footer

--- src/lib/UI/layout/Footer.svelte
+++ src/lib/UI/layout/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer
-    class="m-auto lg:pe-[30%] px-2 md:px-10 text-crema mt-40 bg-bg3 h-16 flex flex-row justify-between items-center"
+    class="relative z-20 m-auto lg:pe-[30%] px-2 md:px-10 text-crema mt-40 bg-bg3 bg-cover h-16 flex flex-row justify-between items-center"
 >
     <p>copyright ©️ 2023</p>
     <a href="mailto:felipeazs@gmail.com" class="md:tracking-wider">
