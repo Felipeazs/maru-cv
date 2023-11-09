fd69f454ec439d7cf0201e444874be7f0d8544d8
felipeazs <felipeazs@gmail.com>
2023-11-09 14:04:43 -0300

    incorporación número de elementos agregados al cv en el trash

--- src/lib/UI/PreviewPDF.svelte
+++ src/lib/UI/PreviewPDF.svelte
@@ -11,7 +11,7 @@
 
 {#if pdfItems.length}
     <div
-        class="absolute bottom-1 right-2 z-30 flex flex-row items-center gap-2"
+        class="absolute bottom-1 right-3 z-30 flex flex-row items-center gap-2"
         transition:fade={{ delay: 0, duration: 200 }}
     >
         <label
@@ -27,10 +27,19 @@
             <label class="modal-backdrop" for="my_modal_6">Close</label>
         </div>
         <button
-            class="btn btn-xs bg-transparent text-white border-2 border-gris hover:bg-transparent hover:border-yellow lowercase transition ease-in-out delay-75 hover:scale-110"
+            class="indicator btn btn-xs bg-transparent text-white border-2 border-gris hover:bg-transparent hover:border-yellow lowercase transition ease-in-out delay-75 hover:scale-110"
             use:tooltipy={{ content: "limpiar cv" }}
             on:click={() => (pdfItems = [])}
         >
+            <span
+                class="indicator-item text-crema align-baseline badge badge-secondary bg-gris border-gris shadow-sm shadow-black w-0 text-xs"
+                transition:fade={{
+                    delay: 0,
+                    duration: 100,
+                }}
+            >
+                {pdfItems.length}
+            </span>
             <img src={trash} alt="el" width={15} height={15} />
         </button>
     </div>
