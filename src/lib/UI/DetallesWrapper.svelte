d7d2ade5acafa12ee8692f6467565826d496dec1
felipeazs <felipeazs@gmail.com>
2023-11-09 13:22:39 -0300

    cambios menores

--- src/lib/UI/DetallesWrapper.svelte
+++ src/lib/UI/DetallesWrapper.svelte
@@ -82,7 +82,7 @@
                             class={item.educacion ||
                             item.proyecto ||
                             item.empresa
-                                ? "indicator z-10 bg-[#fff] text-md text-black font-bold border-2 border-[rgba(78,87,98,0.8)] rounded-lg hover:cursor-pointer transition ease-in-out delay-75 hover:scale-110 hover:border-yellow"
+                                ? "indicator z-10 bg-[#fff] text-md text-black font-bold border-2 border-[rgba(78,87,98,0.8)] rounded-lg hover:cursor-pointer hover:border-yellow"
                                 : "z-10 bg-[#fff] text-md text-black font-bold border-2 border-[rgba(78,87,98,0.8)] rounded-lg hover:cursor-auto"}
                             on:click={() => badgeHandler(item)}
                         >
