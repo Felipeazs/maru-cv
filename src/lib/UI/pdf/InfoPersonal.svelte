451519a45aa394c82f1407c7d809b6460d9958ad
felipeazs <felipeazs@gmail.com>
2024-02-28 08:32:19 -0300

    google scholar

--- src/lib/UI/pdf/InfoPersonal.svelte
+++ src/lib/UI/pdf/InfoPersonal.svelte
@@ -16,19 +16,12 @@
     <span>LinkedIn:</span>
     <span class="col-span-4">memartinezv</span>
     <span>Google Scholar:</span>
-    <span class="col-span-4">
-        <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://scholar.google.es/citations?hl=es&user=yiD0lWEAAAAJ&view_op=list_works&citft=1&citft=2&email_for_op=maru.27%40gmail.com&gmla=AJsN-F4DCW4yLX6oSdj0lM3-60dczH4o2mEe6bqL3c4199cGRXs_Z0-0yTSr5JNnvcyX7rTPYA0-V30qSt2DHN8DTOumSTY_7P77BtiQ-T2ZRq2aUFAuRJH_NEU_LH4esw7V6RGNOlkXtbFwtkvzZfMzcccRwKGCz-YUPJf-DZdQmUHBhyQ82vVrWr5vN7Q4usS92YWvgk3XcJuzSOCGOcNCSGpa3hi-UAx6j3_ZtfAb_K5EJ2ykdPydqZbMBa-siNTP6in-QF_9"
-            >link perfil</a
-        >
-    </span>
+    <span class="col-span-4"> María Eugenia Martínez Valenzuela </span>
     <span>ORCID:</span>
     <span class="col-span-4">0000-0001-8455-7356</span>
-    <span>Scopus Author ID</span>
+    <span>Scopus Author ID:</span>
     <span class="col-span-4">57213207712</span>
-    <span>Researcher ID</span>
+    <span>Researcher ID:</span>
     <span class="col-span-4">HDM-3938-2022</span>
     <span>CV online:</span>
     <span class="col-span-4">https://maru-cv.vercel.app</span>
