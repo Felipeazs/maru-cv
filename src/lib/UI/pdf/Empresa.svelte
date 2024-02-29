5099ffaaa51b4f5c5673b35914ff405b71acf339
felipeazs <felipeazs@gmail.com>
2024-02-29 06:42:26 -0300

    arreglo empresas

--- src/lib/UI/pdf/Empresa.svelte
+++ src/lib/UI/pdf/Empresa.svelte
@@ -9,14 +9,18 @@
         <h3 class="uppercase font-bold">Empresas</h3>
         {#each empresas as item}
             <div class="grid grid-cols-9 grid-auto-rows my-5">
-                <span class="row-span-3 col-span-2">{item.titulo.join(", ")}</span>
+                <span class="row-span-3 col-span-2"
+                    >{item.titulo.join(", ")}</span
+                >
                 <span class="col-span-7 font-semibold">{item.cargo}</span>
-                <span class="col-span-7">{item.descripcion}</span>
+                <span class="col-span-7">{item.descripcion ?? ""}</span>
                 <span class="col-span-7">
                     <ul>
-                        {#each item.mas[0].split("|") as descripcion}
-                            <li class="list-inside">{descripcion}</li>
-                        {/each}
+                        {#if item.mas}
+                            {#each item.mas[0].split("|") as descripcion}
+                                <li class="list-inside">{descripcion}</li>
+                            {/each}
+                        {/if}
                     </ul>
                 </span>
             </div>
